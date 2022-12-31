854790235d104be351f25338bc87faabbea52a13
Grinets <serega1997@yandex.ru>
2022-12-31 03:04:43 +0300

    createdClassGameAsMainObjAndRunnerjsAsStart

--- run.js
+++ run.js
@@ -1,1 +1,8 @@
-console.log(process.env);
+const Game = require('./src/Game');
+
+// Инициализация игры с настройками.
+const game = new Game(30);
+
+
+// Запуск игры.
+game.play();
---
+++ src/Game.js
@@ -0,0 +1,36 @@
+const Hero = require('./game-models/Hero');
+const Enemy = require('./game-models/Enemy');
+const Boomerang = require('./game-models/Boomerang');
+
+const View = require('./View');
+
+// Основной класс игры.
+// Тут будут все настройки, проверки, запуск.
+
+class Game {
+  constructor( trackLength ) {
+    this.trackLength = trackLength;
+    this.hero = new Hero(); 
+    this.enemy = new Enemy();
+    this.view = new View();
+    this.track = [];
+    this.regenerateTrack();
+  }
+
+  regenerateTrack() {
+    // Сборка всего необходимого (герой, враг(и), оружие)
+    // в единую структуру данных
+    this.track = (new Array(this.trackLength)).fill(' ');
+    this.track[this.hero.position] = this.hero.skin;
+  }
+
+  play() {
+    setInterval(() => {
+
+      this.regenerateTrack();
+      this.view.render(this.track);
+    });
+  }
+}
+
+module.exports = Game;
